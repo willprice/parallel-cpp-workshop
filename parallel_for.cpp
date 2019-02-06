3cff601ea32e292ac3c38a19e3fbfec29e9de513
Will Price <will.price94@gmail.com>
2019-02-06 22:04:27 +0000

    Parallelize loop in parallel_for using tbb

--- parallel_for.cpp
+++ parallel_for.cpp
@@ -2,18 +2,23 @@
 #include <vector>
 #include <cmath>
 
+#include <tbb/parallel_for.h>
+
+
 int main(int argc, char **argv)
 {
     auto values = std::vector<double>(10000);
 
-    for (int i=0; i<values.size(); ++i)
-    {
-        values[i] = std::sin(i * 0.001);
-    }
+    tbb::parallel_for(tbb::blocked_range<int>(0, values.size()),
+      [&](tbb::blocked_range<int> r){
+          for (int i = r.begin(); i < r.end(); i++) {
+              values[i] = std::sin(i * 0.001);
+          };
+    });
 
     double total = 0;
 
-    for (double value : values)
+    for (auto value : values)
     {
         total += value;
     }
