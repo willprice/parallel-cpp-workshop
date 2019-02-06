fbcb283b27b391495d02bc597cb0500aec9111f2
Will Price <will.price94@gmail.com>
2019-02-06 16:19:14 +0000

    Use reduce and lambda to sum all line counts

--- countlines.cpp
+++ countlines.cpp
@@ -7,6 +7,8 @@ using namespace filecounter;
 int main(int argc, char** argv) {
     auto file_paths = get_arguments(argc, argv);
     auto line_counts = map(count_lines, file_paths);
+    auto total_line_count = reduce([](int a, int b) { return a + b; }, line_counts);
+    std::cout << "Total line count: " << total_line_count << std::endl;
     for (int i = 0; i < file_paths.size(); i++) {
         std::cout << file_paths[i] << ": " << line_counts[i] << std::endl;
     }
