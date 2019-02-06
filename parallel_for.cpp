3ab1b57b0abd01995973406c39ad09a15717a17f
Will Price <will.price94@gmail.com>
2019-02-06 22:06:22 +0000

    Add logging to parallel_for to show blocked_range sizes

--- parallel_for.cpp
+++ parallel_for.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <mutex>
 
 #include <tbb/parallel_for.h>
 
 
 int main(int argc, char **argv)
 {
+    std::mutex m;
+
     auto values = std::vector<double>(10000);
 
     tbb::parallel_for(tbb::blocked_range<int>(0, values.size()),
       [&](tbb::blocked_range<int> r){
+          m.lock();
+          std::cout << "Range size " << (r.end() - r.begin())
+                    << " from " << r.begin() << " to " << r.end() << std::endl;
+          m.unlock();
+
           for (int i = r.begin(); i < r.end(); i++) {
               values[i] = std::sin(i * 0.001);
           };
